64efe17d4d3cbb5869ab9ccf3668117c7988b7d7
JioBon <jcbonifacio.1217@gmail.com>
2023-04-01 18:01:33 +0800

    Fix get list of stresses and specific stress

--- Crop.py
+++ Crop.py
@@ -2,9 +2,13 @@ class Crop:
     def __init__(self, crop, sci_name, stresses, descriptions, solutions):
         self.crop = crop
         self.sci_name = sci_name
+        self.stresses = stresses
+        self.descriptions = descriptions
+        self.solutions = solutions
+
         self.stress_dict = []
         for i in range(len(stresses)):
-            self.stress_dict.append({"Crop": self.crop, "Stress": stresses[i], "Description": descriptions[i], "Solutions": solutions[i]})
+            self.stress_dict.append({"Crop": self.crop, "Scientific": self.sci_name, "Stress": self.stresses[i], "Description": self.descriptions[i], "Solutions": self.solutions[i]})
 
     def call_crop(self):
         return f"{self.crop}"
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ onion_stressDesc = [
     """Botrytis leaf blight, sometimes also referred to as Botrytis leaf spot, occurs on onions. 
     White, sunken spots on leaves are usually the first sign of infection. Spots are small, oval-shaped, and range from 0.06 to 0.25 inch (0.5–6 mm) long. 
     They sometimes have a light-green halo and may appear water-soaked. The epidermis around the spots may be silvery. 
-    When numerous spots are present, leaf tips die back, and whole leaves may eventually die.""",
+    When numerous spots are present, leaf tips die back, and whole leaves may eventually die.""".strip(),
     """
     At the field level, symptoms of downy mildew are first noticed as circular clumps of yellowed plants that are a few to many feet in diameter. 
     As the disease progresses, the yellowing patterns often enlarge in the direction of prevailing winds.
@@ -18,20 +18,20 @@ onion_stressDesc = [
     When leaves are wet, or when humidity is very high, signs of Peronospora destructor are visible on the surface of older leaves as fine, furry, grayish-white growths. 
     These growths may later turn purple or brown as a result of a secondary infection of the lesion by other fungi, 
     such as those that cause PURPLE BLOTCH AND STEMPYLIUM LEAF BLIGHT, as these fungi can produce purple pigmentation and dark spores.
-    """,
+    """.strip(),
     """
 
-    """,
+    """.strip(),
     """
     Adults have yellow heads, knees, sides of abdomens; otherwise mostly dark gray, just over 1/10 inch long.
     Pupae are red-brown, slightly longer than adults.
     Larvae are white to yellow, up to about 1/3 inch long.
     Eggs are white, about 1/50 inch long, found on leaves.
     Adult feeding holes found in rows, with milky white sap coming out.
-    """
+    """.strip()
     ]
 onion_stressSoln = [
-    ["apply appropriate fungicide sprays"],
+    ["apply appropriate fungicide sprays".strip()],
     ["""
     Spray at the first sign of the disease. After the first spray, scout fields and make subsequent applications when weather conditions are favorable for the disease. 
     Rotate with fungicides from different mode-of-action groups to reduce the risk of fungicide resistance development. 
@@ -39,17 +39,17 @@ onion_stressSoln = [
     For all fungicides, thorough coverage of foliage is important in the control of downy mildew. 
     apply appropriate foliar fungicides taking care to apply thoroughly to waxy leaves. 
     Chlorothalonil and mancozeb are the main protectant fungicides for downy mildew.
-    """],
-    [""],
+    """.strip()],
+    ["".strip()],
     ["""
     Pyrethrin This organic pesticide will kill leaf miners as they leave the egg and enter the leaf. 
     Since they have to chew into the leaf, they ingest the poison with the leaf and die. 
     However, pyrethrin also kills good insects such as bee and leaf miner preditors. 
     Use to spot treat concentrations of leaf miners rather than blanket the whole garden with it. 
     Be sure you get it on the bottom side of the leaf.
-    """]
+    """.strip()]
 ]
-onion_data = Crop("Onion", "Allium cepa", onion_stress, onion_stressDesc, onion_stressSoln)
+onion_data = Crop("Onion", "Allium cepa", onion_stress,  onion_stressDesc, onion_stressSoln)
 
 class Msg(BaseModel):
     msg: str
@@ -62,11 +62,22 @@ async def root():
 
 @app.get("/{crop}")
 async def get_Crop(crop: str):
-    if crop == "Onion":     
-        return onion_data.call_dict
+    to_return = {}
+    if crop == "Onion":
+        for i in len(range(onion_data.stress_dict)):
+            to_return.update({i : onion_data.call_dict[i]})
+        return to_return
     else:
         return {"message": f"{crop} no on the list"}
+    
 
+@app.get("/Onion/{stress}")
+async def get_Onion(stress: str):
+    for i in onion_data.stress_dict:
+        if i['Stress'] == stress:
+            return i
+    else:
+        return {"message": "Stress not found"}
 
 # @app.post("/path")
 # async def demo_post(inp: Msg):
