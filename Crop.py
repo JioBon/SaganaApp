1d67031e978a12d1e140d5f9f6d284f5b6c3db66
JioBon <jcbonifacio.1217@gmail.com>
2023-04-01 18:58:26 +0800

    Change solutions to dict

--- Crop.py
+++ Crop.py
@@ -5,10 +5,13 @@ class Crop:
         self.stresses = stresses
         self.descriptions = descriptions
         self.solutions = solutions
+        self.soln_dict = []
+        for i in range(len(solutions)):
+            self.soln_dict.append({"id": i, "Solution": solutions[i]})
 
         self.stress_dict = []
         for i in range(len(stresses)):
-            self.stress_dict.append({"Crop": self.crop, "Scientific": self.sci_name, "Stress": self.stresses[i], "Description": self.descriptions[i], "Solutions": self.solutions[i]})
+            self.stress_dict.append({"Crop": self.crop, "Scientific": self.sci_name, "Stress": self.stresses[i], "Description": self.descriptions[i], "Solutions": self.soln_dict[i]})
 
     def call_crop(self):
         return f"{self.crop}"
